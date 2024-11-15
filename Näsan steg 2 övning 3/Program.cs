1cb41975b7cb81c384650c6c1d252f7f61383a25
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-15 10:02:33 +0100

    klar

---
+++ Näsan steg 2 övning 3/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] namn = {"Jakob", "Ludvig", "Richard", "Vincent", "Simon"};
+
+string[] träd = {"björkträd", "ekträd", "lönnträd", "tallträd", "barkträd"};
+
+int[] t = {1,3,6,8,10};
+
+
+
+for(int e = 0; e < 5; ++e){
+    Console.WriteLine($"{namn[e]} hoppar från ett {träd[e]} och får betyget {t[e]} av domarna.");
+}
+Console.ReadLine();
